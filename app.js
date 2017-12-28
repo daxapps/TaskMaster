cbde1953c2110e272f9a20a332dd9acb9864ff90
Jason Crawford <daxapps777@gmail.com>
2017-12-28 13:42:26 -0600

    Remove tasks from localStorage

--- app.js
+++ app.js
@@ -93,8 +93,29 @@ function removeTask(e) {
   if (e.target.parentElement.classList.contains('delete-item')) {
     if (confirm('Are You Sure?')) {
       e.target.parentElement.parentElement.remove();
+
+      // remove from localStorage
+      removeTaskFromLocalStorage(e.target.parentElement.parentElement);
     }
   }
+}
+
+// remove from localStorage
+function removeTaskFromLocalStorage(taskItem) {
+  let tasks;
+  if(localStorage.getItem('tasks') === null) {
+    tasks = [];
+  } else {
+    tasks = JSON.parse(localStorage.getItem('tasks'));
+  }
+
+  tasks.forEach(function(task, index) {
+    if(taskItem.textContent === task) {
+      tasks.splice(index, 1);
+    }
+  });
+
+  localStorage.setItem('tasks', JSON.stringify(tasks));
 }
 
 // clear tasks
@@ -102,6 +123,14 @@ function clearTasks(e) {
   while (taskList.firstChild) {
     taskList.removeChild(taskList.firstChild);
   }
+
+  // clear from localStorage
+  clearTasksFromLocalStorage();
+}
+
+// clear tasks from ls
+function clearTasksFromLocalStorage() {
+  localStorage.clear();
 }
 
 // filter tasks
